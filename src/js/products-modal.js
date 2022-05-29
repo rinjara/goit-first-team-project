26c50d74bb3a6ce51b2d2ff186d5ef8c33847aeb
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-05-30 01:15:31 +0300

    products-more-modal

---
+++ src/js/products-modal.js
@@ -0,0 +1,50 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-icecream-modal-open]"),
+    closeModalBtn: document.querySelector("[data-icecream-modal-close]"),
+    modal: document.querySelector("[data-icecream-modal]"),
+   // body: document.querySelector("[data-nonScroll]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+   // refs.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is--hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-icecoffee-modal-open]"),
+    closeModalBtn: document.querySelector("[data-icecoffee-modal-close]"),
+    modal: document.querySelector("[data-icecoffee-modal]"),
+   // body: document.querySelector("[data-nonScroll]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+   // refs.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is--hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-milkshakes-modal-open]"),
+    closeModalBtn: document.querySelector("[data-milkshakes-modal-close]"),
+    modal: document.querySelector("[data-milkshakes-modal]"),
+   // body: document.querySelector("[data-nonScroll]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+   // refs.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is--hidden");
+  }
+})();
