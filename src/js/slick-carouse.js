f67ae9013a3825960aff5fea38a106607f03928d
Dmytro <DSerSmart@gmail.com>
2022-05-30 09:14:45 +0300

    Scroll Time

--- src/js/slick-carouse.js
+++ src/js/slick-carouse.js
@@ -4,7 +4,10 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 100000,
+    autoplaySpeed: 20000,
     dots: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
   });
 });
