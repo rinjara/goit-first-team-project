93dfbad8f8f8cde65551a4f4e546bb913766dce3
rinjara <99882406+rinjara@users.noreply.github.com>
2022-05-31 20:32:41 +0000

    Deploying to gh-pages from @ rinjara/goit-first-team-project@2a2cff6aa1228586d27873e61ab3875e5525a644 🚀

--- index.3f51d145.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".customer__list").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e4,dots:!0,adaptiveHeight:!0,speed:500,fade:!0,cssEase:"linear"})}));
-//# sourceMappingURL=index.3f51d145.js.map
---
+++ index.a4f58a84.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".customer__list").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e4,dots:!0,adaptiveHeight:!0,speed:500,fade:!0,cssEase:"linear"})}));
+//# sourceMappingURL=index.a4f58a84.js.map
