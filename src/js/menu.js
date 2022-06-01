9c5c98696d3c30df03ab2ea87ff2ee9f66c97dc6
Krystyna Sharhorodska <rinjara@gmail.com>
2022-06-01 16:54:55 +0300

    Menu tablet and id fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -18,6 +18,9 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
+  const menuLinks = document.querySelectorAll('.mobile-menu li a')
+  for( const link of menuLinks) link.addEventListener('click', toggleMenu)
+
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
